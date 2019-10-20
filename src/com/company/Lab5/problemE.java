41339714a59730fd50f465b4acd69ddf1760eeea
zy <1164138952@qq.com>
2019-10-20 21:41:43 +0800

    An update

--- src/com/company/Lab5/problemE.java
+++ src/com/company/Lab5/problemE.java
@@ -1,7 +1,107 @@
 package com.company.Lab5;
 
-public class problemE {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class problemE {
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+    }
+
+    private static long[] pow = new long[50000];
+
+    static {
+        long temp = 1;
+        for (int i = 0; i < 50000; i++) {
+            pow[i] = temp;
+            temp *= 139;
+        }
+    }
+
+    private static int halfSearch(long[] arr, long key) {
+        int min = 0, max = arr.length - 1, mid;
+
+        while (min <= max) {
+            mid = (min + max) >> 1;
+            if (key > arr[mid])
+                min = mid + 1;
+            else if (key < arr[mid])
+                max = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        InputReader in = new InputReader(inputStream);
+        String s1 = in.next();
+        String s2 = in.next();
+        if (!(s1.length() >= s2.length())) {
+            String temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+        int min = 0;
+        int max = s2.length();
+
+        while (min < max) {
+            int mid = (min + max + 1) >> 1;
+            long[] s2hash = new long[s2.length() - mid + 1];
+
+            for (int i = 0; i < mid; i++) {
+                s2hash[0] += s2.charAt(mid - 1 - i) * pow[i];
+            }
+
+            for (int i = 1; i < s2.length() - mid + 1; i++) {
+                s2hash[i] = (s2hash[i - 1] - s2.charAt(i - 1) * pow[mid - 1]) * 139 + s2.charAt(i + mid - 1);
+            }
+
+
+            int a = -1;
+            long[] s1hash = new long[s1.length() - mid + 1];
+
+            for (int j = 0; j < mid; j++) {
+                s1hash[0] += s1.charAt(mid - 1 - j) * pow[j];
+            }
+
+            for (int j = 1; j < s1.length() - mid + 1; j++) {
+                s1hash[j] = (s1hash[j - 1] - s1.charAt(j - 1) * pow[mid - 1]) * 139 + s1.charAt(j + mid - 1);
+            }
+            Arrays.sort(s1hash);
+            for (long hash : s2hash) {
+                a = halfSearch(s1hash, hash);
+                if (a != -1) {
+                    break;
+                }
+            }
+            if (a == -1) {
+                max = mid - 1;
+            } else min = mid;
+        }
+        System.out.print(max);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
 
+import java.util.Arrays;
+
 public class Main {
 
 
     public static void main(String[] args) {
-
-        String string = "abcdefg";
-        System.out.println(string.substring(3, string.length()));
+        int[] ints = {5, 4, 23, 231, 434, 3, 0, 0};
+        Arrays.sort(ints);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
     }
 }
