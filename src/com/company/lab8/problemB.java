ffd1a19a4122caacb645ac2258cb262bbf7171bc
zy <1164138952@qq.com>
2019-11-30 10:42:59 +0800

    An update

--- src/com/company/lab8/problemB.java
+++ src/com/company/lab8/problemB.java
@@ -1,6 +1,9 @@
 package com.company.lab8;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -33,34 +36,83 @@ public class problemB {
 
     static class Node {
         ArrayList<Node> friends;
-        int value;
+        int flag = 0;
+        int x;
+        int y;
+        int r;
 
-        Node(int value) {
-            this.value = value;
+        Node(int x, int y, int r) {
+
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            friends = new ArrayList<>();
         }
+
+        void addFriends(Node node) {
+            friends.add(node);
+        }
+    }
+
+    private static Node judge(Node node) {
+
+        if (node.flag == 0)
+            node.flag = 1;
+        if (node.flag == 2)
+            return node;
+        for (int i = 0; i < node.friends.size(); i++) {
+
+            if (node.friends.get(i).flag == 0 || node.friends.get(i).flag == 2) {
+                Node tmp = judge(node.friends.get(i));
+                if (tmp != null)
+                    return tmp;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
 
         InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
-        PrintWriter out = new PrintWriter(outputStream);
         int times = in.nextInt();
         for (int i = 0; i < times; i++) {
 
             int n = in.nextInt();
             int m = in.nextInt();
             int k = in.nextInt();
-            Node start = new Node(0);
-            for (int j = 0; j < k; j++) {
+            Node[] nodes = new Node[k + 2];
+            nodes[0] = new Node(0, 0, 0);
+            nodes[k + 1] = new Node(0, 0, 0);
+            nodes[k + 1].flag = 2;
+            for (int j = 1; j <= k; j++) {
                 int x = in.nextInt();
                 int y = in.nextInt();
                 int s = in.nextInt();
+                nodes[j] = new Node(x, y, s);
+                if (x - s <= 0 || y + s >= m) {
+                    nodes[0].addFriends(nodes[j]);
+                    nodes[j].addFriends(nodes[0]);
+                }
+                if (x + s >= n || y - s <= 0) {
+                    nodes[k + 1].addFriends(nodes[j]);
+                    nodes[j].addFriends(nodes[k + 1]);
+                }
 
+                for (int l = 1; l < j; l++) {
+                    if ((nodes[l].x - nodes[j].x) * (nodes[l].x - nodes[j].x) +
+                            (nodes[l].y - nodes[j].y) * (nodes[l].y - nodes[j].y) -
+                            (nodes[l].r + nodes[j].r) * (nodes[l].r + nodes[j].r) <= 0) {
+                        nodes[l].addFriends(nodes[j]);
+                        nodes[j].addFriends(nodes[l]);
 
+                    }
+                }
             }
 
+            if (judge(nodes[0]) == null)
+                System.out.println("Yes");
+            else System.out.println("No");
         }
     }
 }
---
+++ src/com/company/lab8/problemC.java
@@ -0,0 +1,108 @@
+package com.company.lab8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class problemC {
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class Node {
+        Node next;
+        int value;
+
+        Node(Node next, int value) {
+            this.next = next;
+            this.value = value;
+        }
+
+    }
+
+    static class queue {
+        ArrayList<Node> arrayList;
+
+        queue() {
+            this.arrayList = new ArrayList<>();
+        }
+
+        void enQueue(Node node) {
+            arrayList.add(node);
+        }
+
+        Node deQueue() {
+            Node tmp = arrayList.get(0);
+            arrayList.remove(0);
+            return tmp;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        InputReader in = new InputReader(inputStream);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        Node[] nodes = new Node[400004];
+        int count = n;
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = new Node(null, i);
+        }
+        for (int i = 0; i < m; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int weight = in.nextInt();
+
+            Node tmp;
+            if (nodes[x - 1].next != null) {
+                tmp = nodes[x - 1].next;
+
+                while (tmp.next != null) {
+                    tmp = tmp.next;
+                    System.out.println(i);
+                }
+            } else tmp = nodes[x - 1];
+
+            if (weight == 2) {
+                tmp.next = new Node(null, count);
+                nodes[count].next = new Node(null, y - 1);
+                count++;
+            } else {
+                tmp.next = new Node(null, y - 1);
+            }
+
+        }
+        queue queue = new queue();
+
+
+
+    }
+}
